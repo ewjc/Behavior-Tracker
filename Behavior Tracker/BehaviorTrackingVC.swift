b622dc6cda68f2be5e018493da5465f23611bd71
ewjc <ericswong7@gmail.com>
2016-10-27 13:35:18 -0700

    Added recording function

--- Behavior Tracker/BehaviorTrackingVC.swift
+++ Behavior Tracker/BehaviorTrackingVC.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
-class BehaviorTrackingVC: UIViewController {
-    
+class BehaviorTrackingVC: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+
     @IBOutlet var moodButtons: [UIButton]!
     
     var moodArray = ["😀", "😘", "😞", "😔", "😨", "😭", "😖", "😡"]
@@ -43,13 +44,12 @@ class BehaviorTrackingVC: UIViewController {
         }
         sender.backgroundColor = .blue
     }
-    
-    @IBAction func addAudioRecording(_ sender: UIButton) {
-    }
+
     @IBAction func addVideoRecording(_ sender: UIButton) {
     }
     @IBAction func trackButtonPressed(_ sender: AnyObject) {
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,9 +59,93 @@ class BehaviorTrackingVC: UIViewController {
             
         }
         
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("allowed!")
+                    } else {
+                    let alertMessage = UIAlertController(title: "Error", message: "Failed to record. Please try again later.", preferredStyle: .alert);
+                        alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil));
+                        self.present(alertMessage, animated: true, completion: nil)
+                    return
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+    }
+    
+    //MARK: RECORDING ASPECT OF APP
+
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    @IBAction func recordTapped() {
+        if audioRecorder == nil {
+            recordAudio()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    @IBAction func playTapped(_ sender: UIButton) {
+
     }
     
     
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            let alertMessage = UIAlertController(title: "Success", message: "Your audio recording has been successfully added!", preferredStyle: .alert);
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil));
+            self.present(alertMessage, animated: true, completion: nil)
+            return
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+    
+    func recordAudio() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
     
 }
 
