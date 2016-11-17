bc68968c38cfbd0aa4b1fb286091b5b58af2acdf
ewjc <ericswong7@gmail.com>
2016-11-16 21:29:59 -0800

    goal table view cell

---
+++ Behavior Tracker/GoalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalTableViewCell.swift
+//  Behavior Tracker
+//
+//  Created by Eric Wong on 11/16/16.
+//  Copyright © 2016 Eric Wong. All rights reserved.
+//
+
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
