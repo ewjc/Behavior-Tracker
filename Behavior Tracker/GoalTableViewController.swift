8e9eb84b507848b00f8fbbafd7e43de2d4b59fd3
ewjc <ericswong7@gmail.com>
2016-11-16 21:30:58 -0800

    added tableviews + fixed error

--- Behavior Tracker/GoalTableViewController.swift
+++ Behavior Tracker/GoalTableViewController.swift
@@ -13,11 +13,6 @@ class GoalTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,23 +24,21 @@ class GoalTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
